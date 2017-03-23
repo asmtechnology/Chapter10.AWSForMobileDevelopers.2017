bd5fc467cf633b3f5eecc358b70a03b1cffcd49a
Abhishek Mishra <amishra@asmtechnology.com>
2017-03-23 23:30:41 +0000

    implemented Facebook login and Cognito identity federation

--- Final/AWSChat/AWSChat/LoginViewController.swift
+++ Final/AWSChat/AWSChat/LoginViewController.swift
@@ -15,10 +15,19 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signupButton: UIButton!
     
+    @IBOutlet weak var facebookButton: FBSDKLoginButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.loginButton.isEnabled = false
+        
+        // log out the user if previously logged in.
+        let facebookLoginManager = FBSDKLoginManager()
+        facebookLoginManager.logOut()
+        
+        // set up the information you want to read from the user's Facebook account.
+        facebookButton.readPermissions = ["public_profile", "email"];
     }
 
     override func didReceiveMemoryWarning() {
@@ -119,3 +128,68 @@ extension LoginViewController {
 
 }
 
+
+extension LoginViewController : FBSDKLoginButtonDelegate {
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!,
+                     didCompleteWith result: FBSDKLoginManagerLoginResult!,
+                     error: Error!) {
+        
+        if error != nil {
+            displayLoginError(error: error as NSError)
+            return
+        }
+        
+        if result.isCancelled {
+                return
+        }
+        
+        guard let idToken = FBSDKAccessToken.current() else {
+            let error = NSError(domain: "com.asmtechnology.awschat",
+                                code: 100,
+                                userInfo: ["__type":"Unknown Error", "message":"Facebook JWT token error."])
+            self.displayLoginError(error: error)
+            return
+        }
+        
+        
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me",
+                                                                 parameters: ["fields":"email,name"])
+        
+        graphRequest.start(completionHandler: { (connection, result, error) -> Void in
+            
+            if let error = error {
+                self.displayLoginError(error: error as NSError)
+                return
+            }
+            
+            if let result = result as? [String : AnyObject],
+                let name = result["name"] as? String {
+                
+                let email = result["email"] as? String
+                
+                let indentityPoolController = CognitoIdentityPoolController.sharedInstance
+                indentityPoolController.getFederatedIdentityForFacebook(idToken: idToken.tokenString,
+                    username: name, emailAddress: email,
+                    completion: { (error: Error?) in
+                        
+                        if let error = error {
+                            self.displayLoginError(error:error as NSError)
+                            return
+                        }
+                        
+                        self.displaySuccessMessage()
+                        return
+                })
+                
+            }
+            
+        })
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        // do nothing.
+    }
+    
+}
